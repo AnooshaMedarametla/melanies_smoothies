af9ba0660827edf14f9e4f5b8a89bf7de945a39d
AnooshaMedarametla <105047221+AnooshaMedarametla@users.noreply.github.com>
2024-03-06 14:40:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import snowflake.connector
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
- import requests
+import requests
 
 
 # Write directly to the app
@@ -32,9 +32,9 @@ ingredients_list=st.multiselect(
     ,max_selections=5
 )
    
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
     #st.text(fruityvice_response.json())
-    fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 if ingredients_list:
     
 
